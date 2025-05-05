03aac3f2115414e357ad7dc02369a5830a9234ea
이재석 <jslee01@miridih.com>
2025-05-05 20:02:06 +0900

    feat: 그룹 재배정 함수 초기 구현 - group_assignment_v2.ts 파일 생성하여 reassignmentGroups 함수 기본 구조 추가 - 테스트 코드 업데이트로 실제 기능 구현을 위한 기반 마련 - 빈 배열을 리턴하는 단순 구현으로 기본 테스트 통과하도록 구성

---
+++ functions/check_poll_result/utils/group_assignment_v2.ts
@@ -0,0 +1,4 @@
+export const reassignmentGroups = (bookGroup: any[]) => {
+  return bookGroup;
+};
+
--- tests/check_poll_result/utils/group_assignment_v2_test.ts
+++ tests/check_poll_result/utils/group_assignment_v2_test.ts
@@ -1,5 +1,8 @@
 import { assertEquals } from "jsr:@std/assert";
+import { reassignmentGroups } from "../../../functions/check_poll_result/utils/group_assignment_v2.ts";
 
-Deno.test("일단 통과하는 테스트", (t) => {
-  assertEquals(5, 5);
+Deno.test("그룹 재배정이 일어난 후 책 그룹 목록 결과를 확인할 수 있다.", (t) => {
+  const bookGroups: any[] = [];
+  const assignmentCompletedGroups = reassignmentGroups(bookGroups);
+  assertEquals([], assignmentCompletedGroups);
 })
