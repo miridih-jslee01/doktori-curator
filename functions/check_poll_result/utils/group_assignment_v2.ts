c03c23b722a9fb82a8c9c7f560bc1a6e4e6ef294
이재석 <jslee01@miridih.com>
2025-05-05 20:04:48 +0900

    BookGroup 타입 생성

--- functions/check_poll_result/utils/group_assignment_v2.ts
+++ functions/check_poll_result/utils/group_assignment_v2.ts
@@ -1,4 +1,9 @@
-export const reassignmentGroups = (bookGroup: any[]) => {
+export type BookGroup = {
+  bookTitle: string;
+  members: string[];
+};
+
+export const reassignmentGroups = (bookGroup: BookGroup[]) => {
   return bookGroup;
 };
 
--- tests/check_poll_result/utils/group_assignment_v2_test.ts
+++ tests/check_poll_result/utils/group_assignment_v2_test.ts
@@ -1,8 +1,8 @@
 import { assertEquals } from "jsr:@std/assert";
-import { reassignmentGroups } from "../../../functions/check_poll_result/utils/group_assignment_v2.ts";
+import { BookGroup, reassignmentGroups } from "../../../functions/check_poll_result/utils/group_assignment_v2.ts";
 
 Deno.test("그룹 재배정이 일어난 후 책 그룹 목록 결과를 확인할 수 있다.", (t) => {
-  const bookGroups: any[] = [];
+  const bookGroups: BookGroup[] = [];
   const assignmentCompletedGroups = reassignmentGroups(bookGroups);
   assertEquals([], assignmentCompletedGroups);
 })
